a783296ba65ebcb9fe8014fd4b8aee70efe4f46f
GITMateuszCharczuk <95475613+GITMateuszCharczuk@users.noreply.github.com>
2024-10-09 21:57:37 +0200

    feat: added jQuerry statick import example page: LAB.1.2

---
+++ lab1_pan_les/Pages/querry.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab1_pan_les.Pages
+{
+    public class querry : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public querry(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
