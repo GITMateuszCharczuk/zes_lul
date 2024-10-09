0138cf2e86fc4daeed567fdc86fd96136a9d6748
GITMateuszCharczuk <95475613+GITMateuszCharczuk@users.noreply.github.com>
2024-10-09 20:33:34 +0200

    lab_1_scatch

---
+++ lab1_pan_les/Pages/lubie_placki.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab1_pan_les.Pages
+{
+    public class PlackiModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PlackiModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
