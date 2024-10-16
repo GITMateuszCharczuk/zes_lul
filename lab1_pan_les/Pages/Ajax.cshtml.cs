f3d81445e7174e39d49f2bd253cb18b01f8b98d1
GITMateuszCharczuk <95475613+GITMateuszCharczuk@users.noreply.github.com>
2024-10-16 22:28:13 +0200

    aderfgikjhbsdfrakjihfbg

--- lab1_pan_les/Pages/Ajax.cshtml.cs
+++ lab1_pan_les/Pages/Ajax.cshtml.cs
@@ -1,22 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 namespace lab1_pan_les.Pages
-{ 
+{
     public class AjaxModel : PageModel
     {
         [BindProperty]
         public string Name { get; set; }
-    
+
         [BindProperty]
         public string Email { get; set; }
-
+        
         public IActionResult OnPost()
         {
-            // Handle the submitted form data here
-            // You can add validation, saving to a database, etc.
-
-            // Return a simple JSON response
-            return new JsonResult(new { success = true });
+            // Return the submitted data in the JSON response
+            return new JsonResult(new { success = true, name = Name, email = Email });
         }
     }
 }
